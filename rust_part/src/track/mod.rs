19cc6eb1e2895eaa145753e6fed5a9a7403f3abf
Ya Boy Pax <fleming.paxton@yahoo.com>
2025-08-24 20:57:21 -0700

    fix clearing buffer before sum

--- rust_part/src/track/mod.rs
+++ rust_part/src/track/mod.rs
@@ -163,7 +163,7 @@ impl Track {
 
             self.play_head_mut().position = relative_position;
 
-            //
+             //
             // // Clip
             // for s in buffer.iter_mut() {
             //     *s = (*s).clamp(-1.0, 1.0);
@@ -225,11 +225,6 @@ impl Track {
                 if grains.len() > 64 { grains.remove(0); }
             }
             spawn_ctr = spawn_ctr.saturating_sub(1);
-
-            // clear output
-            for c in 0..channels {
-                buffer[frame * channels + c] = 0.0;
-            }
 
             // advance each grain and sum into output
             grains.retain_mut(|g| {
