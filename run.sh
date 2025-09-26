09d0c26c4751cc2ae66d15e72f8866b72f125c95
Pax Fleming <fleming.paxton@yahoo.com>
2025-09-26 12:20:27 -0700

    fix run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cmake --build cmake-build-release --clean-first -j6 --target Xycles_Standalone
-open /Users/raf-dev/workspace/repos/Xycles/cmake-build-release/Xycles_artefacts/Release/Standalone/Xycles.app
+cmake --build cmake-build-release --clean-first -j6
+open /Users/raf-dev/workspace/repos/Xycles/cmake-build-release/Xycles_artefacts/Release/Xycles.app
