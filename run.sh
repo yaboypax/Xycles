cd31e441ae190f50680ae48d5698ec4f866f9f3a
Pax Fleming <fleming.paxton@yahoo.com>
2025-09-26 15:31:39 -0700

    cross platformize run script

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
 cmake --build cmake-build-release --clean-first -j6
-open /Users/raf-dev/workspace/repos/Xycles/cmake-build-release/Xycles_artefacts/Release/Xycles.app
+
+OS=$(uname)
+case "$OS" in
+   'Darwin') 
+	open  cmake-build-release/Xycles_artefacts/Release/Xycles.app  
+	;;
+   'CYGWIN'*|'MSYS'*|'MINGW'*)
+    	cmake-build-release/Xycles_artefacts/Release/Xycles.exe
+	;;
+  *)
+    echo "Error Detecting OS: $OS"
+    	;;
+esac
+
