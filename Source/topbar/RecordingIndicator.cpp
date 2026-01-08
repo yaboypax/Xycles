28a6348c17bfd4cc47c26035a3f4bbd62aa62e26
Paxton Fleming <fleming.paxton@yahoo.com>
2026-01-07 22:36:37 -0800

    default music library

--- Source/topbar/RecordingIndicator.cpp
+++ Source/topbar/RecordingIndicator.cpp
@@ -3,91 +3,83 @@
 //
 
 #include "RecordingIndicator.h"
+#include "juce_gui_basics/juce_gui_basics.h"
 
-static std::unique_ptr<OutputStream> makeOutputStream (const URL& url)
-{
-    if (const auto doc = AndroidDocument::fromDocument (url))
-        return doc.createOutputStream();
+static std::unique_ptr<OutputStream> makeOutputStream(const URL &url) {
+  if (const auto doc = AndroidDocument::fromDocument(url))
+    return doc.createOutputStream();
 
-#if ! JUCE_IOS
-    if (url.isLocalFile())
-        return url.getLocalFile().createOutputStream();
+#if !JUCE_IOS
+  if (url.isLocalFile())
+    return url.getLocalFile().createOutputStream();
 #endif
 
-    return url.createOutputStream();
+  return url.createOutputStream();
 }
 
-RecordingIndicator::RecordingIndicator()
-{
-    setOpaque (false);
-    addAndMakeVisible (m_recordingLabel);
-    m_recordingLabel.setFont (FontOptions (32.0f, Font::plain));
-    m_recordingLabel.setJustificationType (Justification::centredTop);
-    m_recordingLabel.setEditable (false, false, false);
-    m_recordingLabel.setColour (Label::textColourId, Colours::red);
+RecordingIndicator::RecordingIndicator() {
+  setOpaque(false);
+  addAndMakeVisible(m_recordingLabel);
+  m_recordingLabel.setFont(FontOptions(32.0f, Font::plain));
+  m_recordingLabel.setJustificationType(Justification::centredTop);
+  m_recordingLabel.setEditable(false, false, false);
+  m_recordingLabel.setColour(Label::textColourId, Colours::red);
 }
 
-void RecordingIndicator::paint (Graphics& g)
-{
-    g.fillAll (Colours::transparentWhite);
+void RecordingIndicator::paint(Graphics &g) {
+  g.fillAll(Colours::transparentWhite);
 }
 
-void RecordingIndicator::resized()
-{
-    m_recordingLabel.setBounds (getLocalBounds().reduced (8));
+void RecordingIndicator::resized() {
+  m_recordingLabel.setBounds(getLocalBounds().reduced(8));
 }
 
-void RecordingIndicator::startRecording()
-{
-    if (! RuntimePermissions::isGranted (RuntimePermissions::writeExternalStorage))
-    {
-        SafePointer<RecordingIndicator> safeThis (this);
+void RecordingIndicator::startRecording() {
+  if (!RuntimePermissions::isGranted(
+          RuntimePermissions::writeExternalStorage)) {
+    SafePointer<RecordingIndicator> safeThis(this);
 
-        RuntimePermissions::request (RuntimePermissions::writeExternalStorage,
-                                     [safeThis] (bool granted) mutable
-                                     {
-                                         if (granted)
-                                             safeThis->startRecording();
-                                     });
-        return;
-    }
+    RuntimePermissions::request(RuntimePermissions::writeExternalStorage,
+                                [safeThis](bool granted) mutable {
+                                  if (granted)
+                                    safeThis->startRecording();
+                                });
+    return;
+  }
 
-    #if (JUCE_ANDROID || JUCE_IOS)
-        auto parentDir = File::getSpecialLocation (File::tempDirectory);
-    #else
-        auto parentDir = File::getSpecialLocation (File::userDocumentsDirectory);
-    #endif
+#if (JUCE_ANDROID || JUCE_IOS)
+  auto parentDir = File::getSpecialLocation(File::tempDirectory);
+#else
+  auto parentDir = File::getSpecialLocation(File::userDocumentsDirectory);
+#endif
 
-    m_lastRecording = parentDir.getNonexistentChildFile ("JUCE Demo Audio Recording", ".wav");
-    m_recorder->startRecording (m_lastRecording);
+  m_lastRecording =
+      parentDir.getNonexistentChildFile("JUCE Demo Audio Recording", ".wav");
+  m_recorder->startRecording(m_lastRecording);
 }
 
-
-void RecordingIndicator::stopRecording()
-{
-    if (m_recorder->isRecording())
-    {
-        m_recorder->stop();
-        m_chooser.launchAsync (  FileBrowserComponent::saveMode
-                         | FileBrowserComponent::canSelectFiles
-                         | FileBrowserComponent::warnAboutOverwriting,
-                         [this] (const FileChooser& c)
-                         {
-                             if (FileInputStream inputStream (m_lastRecording); inputStream.openedOk())
-                                 if (const auto outputStream = makeOutputStream (c.getURLResult()))
-                                     outputStream->writeFromInputStream (inputStream, -1);
-                          });
-    }
+void RecordingIndicator::stopRecording() {
+  if (m_recorder->isRecording()) {
+    m_recorder->stop();
+    const auto _flags = FileBrowserComponent::saveMode |
+                        FileBrowserComponent::canSelectFiles |
+                        FileBrowserComponent::warnAboutOverwriting;
+    m_chooser.launchAsync(_flags, [this](const FileChooser &c) {
+      if (FileInputStream inputStream(m_lastRecording); inputStream.openedOk())
+        if (const auto outputStream = makeOutputStream(c.getURLResult()))
+          outputStream->writeFromInputStream(inputStream, -1);
+    });
+  }
 }
 
-void RecordingIndicator::setRecorder(std::shared_ptr<AudioRecorder> recorder)
-{
-    m_recorder = std::move(recorder);
+void RecordingIndicator::setRecorder(std::shared_ptr<AudioRecorder> recorder) {
+  m_recorder = std::move(recorder);
 }
 
-bool RecordingIndicator::isRecording() const{
+bool RecordingIndicator::isRecording() const {
 
-    if (m_recorder)
-        return m_recorder->isRecording();
-    else return false;
+  if (m_recorder)
+    return m_recorder->isRecording();
+  else
+    return false;
 }
--- Source/topbar/RecordingIndicator.h
+++ Source/topbar/RecordingIndicator.h
@@ -3,30 +3,31 @@
 //
 #pragma once
 
-#include <JuceHeader.h>
 #include "../AudioRecorder.h"
+#include <JuceHeader.h>
 
-class RecordingIndicator final : public juce::Component
-{
+class RecordingIndicator final : public juce::Component {
 public:
-    RecordingIndicator();
+  RecordingIndicator();
 
-    void paint (Graphics& g) override;
-    void resized() override;
+  void paint(Graphics &g) override;
+  void resized() override;
 
-    void startRecording();
-    void stopRecording();
-    void setRecorder(std::shared_ptr<AudioRecorder> recorder);
-    bool isRecording() const;
-
+  void startRecording();
+  void stopRecording();
+  void setRecorder(std::shared_ptr<AudioRecorder> recorder);
+  bool isRecording() const;
 
 private:
-    AudioDeviceManager m_audioDeviceManager;
-    std::shared_ptr<AudioRecorder> m_recorder;
+  AudioDeviceManager m_audioDeviceManager;
+  std::shared_ptr<AudioRecorder> m_recorder;
 
-    Label m_recordingLabel { {},"Recording..." };
-    File m_lastRecording;
-    FileChooser m_chooser { "Output file...", File::getCurrentWorkingDirectory().getChildFile ("recording.wav"), "*.wav" };
+  Label m_recordingLabel{{}, "Recording..."};
+  File m_lastRecording;
+  FileChooser m_chooser{"Output file...",
+                        File::getSpecialLocation(File::userMusicDirectory)
+                            .getChildFile("recording.wav"),
+                        "*.wav"};
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RecordingIndicator)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RecordingIndicator)
 };
