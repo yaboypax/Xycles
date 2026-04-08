21e625cb59c7d61e794b2de6d72f59d429eadcff
Ya Boy Pax <fleming.paxton@yahoo.com>
2026-04-07 18:26:35 -0700

    remove cmake copy step

--- rust_part/build.rs
+++ rust_part/build.rs
@@ -1,4 +1,23 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
-    let _build = cxx_build::bridge("src/lib.rs");
+    cxx_build::bridge("src/lib.rs")
+        .compile("rust_part");
+
     println!("cargo:rerun-if-changed=src/lib.rs");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let generated_header = out_dir
+        .join("cxxbridge")
+        .join("include")
+        .join("rust_part")
+        .join("src")
+        .join("lib.rs.h");
+
+    let stable_header = PathBuf::from("rust_part.h");
+    fs::copy(&generated_header, &stable_header).unwrap();
+
 }
