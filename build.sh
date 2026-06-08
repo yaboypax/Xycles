2a5af4a7dc200785e9677965ab3b84ebb7574cc4
Ya Boy Pax <fleming.paxton@yahoo.com>
2026-06-08 12:18:07 -0700

    drop comment from build.sh -j change

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-# Cap parallel jobs: JUCE + MSVC LTO can use ~1-2 GB per cl.exe, and the
-# default of one-job-per-core OOMs/freezes machines with many cores.
-cmake --build . --config Release -- -j "${JOBS:-6}"
+cmake --build . --config Release -- -j 6
 
 OS=$(uname)
 case "$OS" in
